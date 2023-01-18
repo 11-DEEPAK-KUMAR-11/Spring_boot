7f148b5b26c4a8dd3a810c14046b35fa922e7555
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-01-18 22:19:54 +0530

    Book Liberary app using spring boot without spring data JPA and database

--- App3_BookApp_springBoot/src/main/java/com/book/controller/BookController.java
+++ App3_BookApp_springBoot/src/main/java/com/book/controller/BookController.java
@@ -3,9 +3,12 @@ package com.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.models.Book;
@@ -17,6 +20,7 @@ public class BookController {
 	@Autowired
 	BookService bookService;
 	
+	//get all book handler
 	@GetMapping("/books")
 	public List<Book> getBooks()
 	{
@@ -28,6 +32,7 @@ public class BookController {
 		return book;
 	}
 	
+	//get book by id handler
 	@PostMapping("/books/{id}")
 	public Book getBookById(@PathVariable ("id") int id)
 	{
@@ -35,6 +40,32 @@ public class BookController {
 		
 		return book;
 	}
+	
+	//create book handler
+	@PostMapping("/books")
+	public Book addBook(@RequestBody Book book)
+	{
+		Book newbook=bookService.addBook(book);
+		
+		return newbook;
+	}
+	
+	//delete book by id handler
+	@DeleteMapping("/books/{bookId}")
+	public void deleteBook(@PathVariable ("bookId") int id)
+	{
+		bookService.deleteBookById(id);
+		
+	}
+	
+	//update by id handler
+	@PutMapping("/books/{bookId}")
+	public Book updateBook(@RequestBody Book book,@PathVariable("bookId") int bookId)
+	{
+		bookService.updateBook(book,bookId);
+		return book;
+	}
+	
 	
 	
 	
--- App3_BookApp_springBoot/src/main/java/com/book/service/BookService.java
+++ App3_BookApp_springBoot/src/main/java/com/book/service/BookService.java
@@ -2,6 +2,7 @@ package com.book.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -33,9 +34,39 @@ public class BookService {
 		return book;
 	}
 	
+	//add new book
 	
+	public Book addBook(Book newBook)
+	{
+		list.add(newBook);
+		
+		return newBook;
+	}
 	
-	
+	// delete book by id
+	public void deleteBookById(int id)
+	{
+		//books id match with given id leave this book and add other book in the same list by doing so 
+		//book with given id will be not there in the list----> when filter will return false which means
+		//book id is not matched with given id then collect it and add to the list.
+		list=list.stream().filter(book -> book.getId()!=id).collect(Collectors.toList());
+		System.out.println("Book is deleted with id "+id);
+	}
+
+	//update book details
+	public void updateBook(Book book, int bookId) 
+	{
+       list=list.stream().map(b ->{
+    	   if(b.getId()==bookId)
+    	   {
+    		   b.setTitle(book.getTitle() );
+    		   b.setAuthor(book.getAuthor());
+    	   }
+    	   return b;
+       }).collect(Collectors.toList());
+
+		
+	}
 	
 	
 	
