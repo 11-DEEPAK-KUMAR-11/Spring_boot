94a49843b3290ad8ab7cfdb9ebbddc499ebaf29c
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-01-19 17:28:16 +0530

    get all books handler completed

--- App4_BookApp_springBoot_withJPA_MySql/src/main/java/com/book/controller/BookController.java
+++ App4_BookApp_springBoot_withJPA_MySql/src/main/java/com/book/controller/BookController.java
@@ -1,13 +1,33 @@
 package com.book.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.book.models.Book;
 import com.book.service.BookService;
 
 @RestController
+@RequestMapping("/book")
 public class BookController {
 
 	@Autowired
 	BookService bService;
+	
+	@GetMapping("/books")
+	public ResponseEntity<List<Book>> getAllBooks()
+	{
+		List<Book> books=bService.getAllBooks();
+		return new ResponseEntity<>(books,HttpStatus.ACCEPTED);
+	}
+	
+	
+	
+	
+	
 }
