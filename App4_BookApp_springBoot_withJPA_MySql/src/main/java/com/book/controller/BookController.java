d9e4904ff5bbf1da1a271dffa97881c1c58ec148
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-01-19 19:24:41 +0530

    Get book by id handler completed

--- App4_BookApp_springBoot_withJPA_MySql/src/main/java/com/book/controller/BookController.java
+++ App4_BookApp_springBoot_withJPA_MySql/src/main/java/com/book/controller/BookController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.book.exception.BookException;
 import com.book.models.Book;
 import com.book.service.BookService;
 
@@ -26,6 +28,13 @@ public class BookController {
 		return new ResponseEntity<>(books,HttpStatus.ACCEPTED);
 	}
 	
+    @GetMapping("/books/id")
+    public ResponseEntity<Book> getBookById(@PathVariable ("id") int id) throws BookException
+    {
+    	Book book=bService.getBookById(id);
+    	
+    	return new ResponseEntity<>(book,HttpStatus.OK);
+    }
 	
 	
 	
